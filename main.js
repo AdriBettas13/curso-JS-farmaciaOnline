9912219a0feb901e8c20ad14d9b026a7c2728095
ChechoBettas <adribettas13@hotmail.com>
2023-11-21 21:22:46 -0300

    Se corrigen errores, se modifica JS, se agrega css, se agrega promesas

--- main.js
+++ main.js
@@ -1,59 +1,128 @@
+const login = document.getElementById("login");
+const registro = document.getElementById("seccionRegistro");
+const estadoRegistro = document.getElementById("estadoRegistro");
+const estadoSesion = document.getElementById("estadoSesion");
+const bienvenido = document.getElementById("bienvenido");
 
-document.getElementById("btnRegistro").addEventListener("click", function() {
-  const login = document.getElementById("login");
-  const registro = document.getElementById("seccionRegistro");
+const usuarios = obtenerUsuarios() || [];
 
-  if (login && registro) {
-    login.style.display = "none";
-    registro.style.display = "block";
-  }
-});
-
-const usuario = [];
+function obtenerUsuarios() {
+  return JSON.parse(localStorage.getItem("usuarios")) || [];
+}
 
 function registrarUsuario() {
   const nuevoUsuario = {
-    usuario: document.getElementById("nuevoUsuario").value,
+    nombre: document.getElementById("nuevoNombre").value,
+    apellido: document.getElementById("nuevoApellido").value,
     correo: document.getElementById("correo").value,
     direccion: document.getElementById("direccion").value,
     contraseña: document.getElementById("nuevaContraseña").value,
   };
 
- 
-  if (!nuevoUsuario.usuario || !nuevoUsuario.correo || !nuevoUsuario.direccion || !nuevoUsuario.contraseña) {
-    document.getElementById("estadoRegistro").innerText = "Por favor, complete todos los campos.";
-    return; 
+  if (!nuevoUsuario.nombre || !nuevoUsuario.apellido  || !nuevoUsuario.correo || !nuevoUsuario.direccion || !nuevoUsuario.contraseña) {
+    estadoRegistro.innerText = "Por favor, complete todos los campos.";
+    estadoRegistro.classList.add("texto-rojo");
+    estadoRegistro.classList.remove("texto-verde");
+    return;
   }
 
-  const usuarioExistente = usuario.find(user => user.usuario === nuevoUsuario.usuario);
+  if (nuevoUsuario.nombre.length < 4 || nuevoUsuario.nombre.length > 11) {
+    estadoRegistro.innerText = "El nombre debe tener entre 4 y 11 caracteres.";
+    estadoRegistro.classList("texto-rojo");
+    estadoRegistro.classList.remove("texto-verde");
+    return;
+  }
+
+  if (nuevoUsuario.contraseña.length < 8) {
+    estadoRegistro.innerText = "La contraseña debe tener al menos 8 caracteres.";
+    estadoRegistro.classList.add("texto-rojo");
+    estadoRegistro.classList.remove("texto-verde");
+    return;
+  }
+
+  const usuarioExistente = usuarios.find(user => user.usuario === nuevoUsuario.nombre);
   if (usuarioExistente) {
-    document.getElementById("estadoRegistro").innerText = "El usuario ya existe.";
+    estadoRegistro.innerText = "El usuario ya existe.";
+    estadoRegistro.classList.add("texto-rojo");
+    estadoRegistro.classList.remove("texto-verde");
   } else {
-    usuario.push(nuevoUsuario);
-    document.getElementById("estadoRegistro").innerText = `Usuario Registrado: Nombre: ${nuevoUsuario.usuario} / Correo: ${nuevoUsuario.correo} / Direccion: ${nuevoUsuario.direccion} / Contraseña: ${nuevoUsuario.contraseña}`;
+    usuarios.push(nuevoUsuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    estadoRegistro.innerText = `Usuario Registrado: Nombre: ${nuevoUsuario.nombre} / Correo: ${nuevoUsuario.correo} / Direccion: ${nuevoUsuario.direccion} / Contraseña: ${nuevoUsuario.contraseña}`;
+    estadoRegistro.classList.add("texto-verde");
+    estadoRegistro.classList.remove("texto-rojo");
   }
 }
+
+const btnRegistro = document.getElementById("btnRegistro");
+
+btnRegistro.addEventListener("click", function() {
+  registro.style.display = "block";
+  login.style.display = "none";
+});
+
+const btnSesionMostrar = document.getElementById("btnSesionInicio");
+
+btnSesionMostrar.addEventListener("click", function() {
+  login.style.display = "block";
+  registro.style.display = "none";
+});
+
+function iniciarSesionPromise(correo, contra) {
+  return new Promise((resolve, reject) => {
+    const usuarioValido = usuarios.find(user => user.correo === correo && user.contraseña === contra);
+
+    if (usuarioValido) {
+      localStorage.setItem("usuarioActual", JSON.stringify(usuarioValido));
+      localStorage.setItem("usuarioAutenticado", true);
+
+      setTimeout(() => {
+        resolve(`Inicio de sesión exitoso. ¡Bienvenido ${usuarioValido.apellido}, ${usuarioValido.nombre}!`);
+      }, 2000);
+    } else {
+      reject("Usuario incorrecto. Inténtelo de nuevo");
+    }
+  });
+}
+
+const correoInput = document.getElementById("usuario");
+const contraInput = document.getElementById("contraseña");
 
 function iniciarSesion() {
-  document.getElementById("seccionRegistro").style.display = "none";
-  document.getElementById("login").style.display = "block";
+  const correo = correoInput.value;
+  const contra = contraInput.value;
+
+  iniciarSesionPromise(correo, contra)
+    .then(mensaje => {
+      estadoSesion.innerText = mensaje;
+      estadoSesion.classList.add("texto-verde");
+      estadoSesion.classList.remove("texto-rojo");
+
+      setTimeout(() => {
+        window.location.href = 'medicamentos.html';
+      }, 2000);
+    })
+    .catch(error => {
+      estadoSesion.innerText = error;
+      estadoSesion.classList.add("texto-rojo");
+      estadoSesion.classList.remove("texto-verde");
+    });
 }
 
-function login() {
-  const usuar = document.getElementById("usuario").value
-  const contra = document.getElementById("contraseña").value
-  const usuarioValido = usuario.find(user => user.usuario === usuar && user.contraseña === contra);
+document.addEventListener("DOMContentLoaded", function() {
+  const usuarioAutenticado = JSON.parse(localStorage.getItem("usuarioAutenticado")) || false;
+  const usuarioActual = JSON.parse(localStorage.getItem("usuarioActual"));
 
-  if (usuarioValido) {
-    document.getElementById("estadoSesion").innerText = `Inicio de sesión existoso. ¡Bienvenido!`;
-    setTimeout(function(){
-
+  if (usuarioAutenticado && usuarioActual) {
+    bienvenido.innerText = `¡Bienvenido ${usuarioActual.apellido}, ${usuarioActual.nombre} ! Espere unos segundos y sera dirigido a la pagina de compra`;
+    bienvenido.classList.add("texto-verde");
+    bienvenido.style.display = "block";
+    login.style.display = "none";
+    registro.style.display = "none";
+    setTimeout(() => {
       window.location.href = 'medicamentos.html';
-
-    }, 2000);
-
-    
+    }, 3000);
   } else {
-    document.getElementById("estadoSesion").innerText = `Usuario incorrecto. Intentelo de nuevo`;
+    login.style.display = "block";
   }
-}
+});
--- medicamentos.js
+++ medicamentos.js
@@ -1,24 +1,32 @@
 const medicamentos = [
-    {droga: "Paracetamol", nombre: "Paracetamor", indicacion: "Analgésico", dosis: 500, cantidad: 40, precio: 1700}, 
-    {droga: "Ibuprofeno", nombre: "Ibupofreno", indicacion: "Analgésico", dosis: 600, cantidad: 40, precio: 2000},
-    {droga: "Amoxicilina", nombre: "Mosicilina", indicacion: "Antibiótico", dosis: 500, cantidad: 21, precio: 1200},
-    {droga: "Cefalexina", nombre: "Cefaselina", indicacion: "Antibiótico", dosis: 500, cantidad: 28, precio: 1400},
-    {droga: "Diclofenac", nombre: "Diclofená", indicacion: "Analgésico",  dosis: 75, cantidad: 20, precio: 1300},
-  ];
+    { droga: "Paracetamol", nombre: "Paracetamor", indicacion: "Analgésico", dosis: 500, cantidad: 40, precio: 1700 },
+    { droga: "Ibuprofeno", nombre: "Ibupofreno", indicacion: "Analgésico", dosis: 600, cantidad: 40, precio: 2000 },
+    { droga: "Amoxicilina", nombre: "Mosicilina", indicacion: "Antibiótico", dosis: 500, cantidad: 21, precio: 1200 },
+    { droga: "Cefalexina", nombre: "Cefaselina", indicacion: "Antibiótico", dosis: 500, cantidad: 28, precio: 1400 },
+    { droga: "Diclofenac", nombre: "Diclofená", indicacion: "Analgésico", dosis: 75, cantidad: 20, precio: 1300 },
+];
 
 const medicamentosJSON = JSON.stringify(medicamentos);
 localStorage.setItem("medicamentos", medicamentosJSON);
 
-const carrito = [];
+let carrito = [];
 let total = 0;
 const carritoContenido = document.getElementById("carritoContenido");
 const carritoTotal = document.getElementById("carritoTotal");
-const busquedaInput = document.getElementById("busqueda");
 const resultados = document.getElementById("resultados");
+const busquedaInput = document.getElementById("busqueda");
 const botonBuscar = document.getElementById("botonBuscar");
 const botonBorrar = document.getElementById("botonBorrar");
 
-realizarBusqueda();
+document.addEventListener("DOMContentLoaded", function () {
+    realizarBusqueda();
+
+    const carritoAlmacenado = localStorage.getItem("carrito");
+    if (carritoAlmacenado) {
+        carrito = JSON.parse(carritoAlmacenado);
+        actualizarTotal();
+    }
+});
 
 function realizarBusqueda() {
     let buscar = busquedaInput.value.toLowerCase();
@@ -30,6 +38,7 @@ function realizarBusqueda() {
 
     medicamentosFiltrados.forEach(medicamento => {
         let div = document.createElement("div");
+        div.classList.add("medicamento");
         div.innerHTML = `
             <h2>${medicamento.nombre}</h2>
             <p>Droga: ${medicamento.droga}</p>
@@ -49,10 +58,15 @@ function realizarBusqueda() {
 function agregarAlCarrito(precio, droga) {
     const cantidadInput = document.getElementById(`cantidad-${droga}`);
     const cantidad = parseInt(cantidadInput.value);
-    if (cantidad >= 1 && cantidad <= 5) {
+
+    const medicamentoEnCarrito = carrito.find(item => item.droga === droga);
+    if (medicamentoEnCarrito) {
+        medicamentoEnCarrito.cantidad += cantidad;
+    } else {
         carrito.push({ precio, droga, cantidad });
-        actualizarTotal();
     }
+
+    actualizarTotal();
 }
 
 function actualizarTotal() {
@@ -65,13 +79,16 @@ function actualizarTotal() {
 
     total = carrito.reduce((suma, item) => suma + item.precio * item.cantidad, 0);
     carritoTotal.innerText = `Total: $${total.toFixed(2)}`;
+
+    const carritoJSON = JSON.stringify(carrito);
+    localStorage.setItem("carrito", carritoJSON);
 }
 
 function borrarCarrito() {
     carrito.length = 0;
+    localStorage.removeItem("carrito");
     actualizarTotal();
 }
-
 
 busquedaInput.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
@@ -85,3 +102,51 @@ botonBorrar.addEventListener("click", function () {
     busquedaInput.value = "";
     resultados.textContent = "";
 });
+
+function mostrarCarrito() {
+    const carritoSeleccionado = document.getElementById("carritoSeleccionado");
+
+    carritoSeleccionado.innerHTML = "";
+    carrito.forEach(item => {
+        let div = document.createElement("div");
+        div.innerHTML = `<p class="estilo-carrito">${item.droga} - Cantidad: ${item.cantidad} - $${(item.precio * item.cantidad).toFixed(2)}</p>`;
+        
+        carritoSeleccionado.appendChild(div);
+    });
+
+    const carritoContenedor = document.getElementById("carritoContenedor");
+    carritoContenedor.style.display = "block";
+}
+
+function validarCompra() {
+    const usuarioActual = JSON.parse(localStorage.getItem("usuarioActual"));
+    const direccionUsuario = usuarioActual ? usuarioActual.direccion : "Dirección no disponible";
+    const apellidoUsuario = usuarioActual ? usuarioActual.apellido : "Apellido no disponible";
+    const nombreUsuario = usuarioActual ? usuarioActual.nombre : "Nombre no disponible";
+
+    const mensajeCompra = document.getElementById("mensajeCompra");
+
+    if (carrito.length > 0) {
+        mensajeCompra.innerText = `¡Compra validada! El pedido será enviado a la siguiente dirección: ${direccionUsuario}, a Nombre de: ${apellidoUsuario}, ${nombreUsuario}`;
+        mensajeCompra.classList.remove("texto-rojo");
+        mensajeCompra.classList.add("texto-verde");
+        carrito.length = 0;
+        localStorage.removeItem("carrito");
+        actualizarTotal();
+
+        
+        setTimeout(function () {
+            mensajeCompra.innerText = "";
+        }, 2000);
+    } else {
+        mensajeCompra.innerText = "El carrito se encuentra vacío.";
+        mensajeCompra.classList.remove("texto-verde");
+        mensajeCompra.classList.add("texto-rojo");
+        setTimeout(function () {
+            mensajeCompra.innerText = "";
+        }, 2000);
+    }
+
+    const carritoContenedor = document.getElementById("carritoContenedor");
+    carritoContenedor.style.display = "none";
+}
